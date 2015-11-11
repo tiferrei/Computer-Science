b07e4a9ce963ea5dfed9a8dbcca444e03b65c672
Tiago Ferreira <tiferrei2000@gmail.com>
2015-11-11 12:41:39 +0000

    More options fore answers (BinarySearchGame.py)

--- BinarySearchGame.py
+++ BinarySearchGame.py
@@ -8,7 +8,8 @@
 import sys      #Used to exit the program at the end.
 import time     #Used to give delay on restarting the program.
 
-
+positiveOptions = ["YES","yes","Yes","y","Y","Yup","yup","sim","s"]
+higherOptions = ["Higher","H","h","High","high"]
 guessed = False    #Creating default values for vital variables.
 maximum = 1000
 minimum = 1
@@ -21,20 +22,20 @@ while wantsToPlay == True:  #Start of the program itslef,
         middle = int(((maximum - minimum) / 2) + minimum) #Calculate the middle point for each maximum and minimum.
         print("Is yor number", str(middle) + "?") #Print data for the user.
         IsMiddle = input("(YES, NO): ") #Ask data from the user, (to know if the number is the one in the middle).
-        if IsMiddle == "YES": #If the number is the one in the middle then do this.
+        if IsMiddle in positiveOptions: #If the number is the one in the middle then do this.
             guessed = True #Mark as guessed to end the loop.
             print("Hurray! I did it!") #Celebrate :)
         else:                        #Else if the number is not the one in the middle do this.
             print("Is your number higher or lower than", str(middle) + "?") #Print information to the user.
             HigherOrLower = input("(Higher, Lower): ")  #Ask for data from the user.
-            if HigherOrLower == "Higher":  #Compare the data from the user to the 2 options. If true then:
+            if HigherOrLower in higherOptions:  #Compare the data from the user to the 2 options. If true then:
                 minimum = (middle + 1)  #The minimum is set to the current middle plus one.
             else:                       #If not:
                 maximum = (middle - 1)  #The maximum is set to the current middle minus one.
 
     PlayAgain = input("Would you like to play again? (YES, NO): ") #Ask the user if he/she wnats to play again.
 
-    if PlayAgain == "YES": #Compare the answer from the user to th 2 options. If he/she wants to play again then:
+    if PlayAgain in positiveOptions: #Compare the answer from the user to th 2 options. If he/she wants to play again then:
         time.sleep(1)   #Wait one second
         guessed = False #Set guessed as False again to re-enter the loop.
     else:               #If not:
