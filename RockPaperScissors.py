bfb63a87dc3415949a773a7f67adf3cd4f394b23
Tiago Ferreira <tiferrei2000@gmail.com>
2015-12-02 13:55:00 +0000

    RockPaperScissors updated

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -16,14 +16,20 @@ Paper = ["P", "p", "Paper", "paper"]
 Rock = ["R", "r", "Rock", "rock"]
 Scissors = ["S", "s", "Scissors", "scissors"]
 
+UserScore = 0
+ComputerScore = 0
+UserTotal = 0
+ComputerTotal = 0
+
 print("Welcome user, to the Rock Paper Scissors game!")
+numberOfRounds = int(input("How many rounds would you like to play? (1, 20): "))
 
-while 1 == 1:
+WantsToPlay = True
+while WantsToPlay == True:
 
-    RandValue = random.randint(0,2)
-    ComputerChoice = ComputerOptions[RandValue]
 
-    IsReady = input("Are you ready? ") #Ask if the user is ready.
+
+    IsReady = input("OK, are you ready? ") #Ask if the user is ready.
     if IsReady in positiveOptions: #If yes then start the countdown.
         print("Starting game in...")
         print("3")
@@ -38,51 +44,59 @@ while 1 == 1:
         break #Break the loop.
         sys.exit() #Exit the program.
 
-    UserInput = input("Paper, Rock or Scissors? ")
-    if UserInput in Paper:
-        UserChoice = "Paper"
-    elif UserInput in Rock:
-        UserChoice = "Rock"
-    elif UserInput in Scissors:
-        UserChoice = "Scissors"
-    else:
-        print("Don't try to fool me.")
-        time.sleep(0.5)
-        break
-        sys.exit()
+    for x in range(0,numberOfRounds):
 
-    if UserChoice == ComputerChoice:
-        print("Tie!")
+        RandValue = random.randint(0,2)
+        ComputerChoice = ComputerOptions[RandValue]
+
+        UserInput = input("Paper, Rock or Scissors? ")
+        if UserInput in Paper:
+            UserChoice = "Paper"
+        elif UserInput in Rock:
+            UserChoice = "Rock"
+        elif UserInput in Scissors:
+            UserChoice = "Scissors"
+        else:
+            print("Don't try to fool me.")
+            time.sleep(0.5)
+            break
+            sys.exit()
+
+        if UserChoice == ComputerChoice:
+            print("Tie!")
+            time.sleep(0.5)
+        elif UserChoice == "Rock" and ComputerChoice == "Paper":
+            print("I won!")
+            ComputerScore = ComputerScore + 1
+            time.sleep(0.5)
+        elif UserChoice == "Rock" and ComputerChoice == "Scissors":
+            print("You won!")
+            time.sleep(0.5)
+            UserScore = UserScore + 1
+        elif UserChoice == "Paper" and ComputerChoice == "Rock":
+            print("You won!")
+            time.sleep(0.5)
+            UserScore = UserScore + 1
+        #elif UserChoice == "Paper" and ComputerChoice == "Scissors":
+        #    print("I won!")
+        #    time.sleep(0.5)
+        #    ComputerScore = ComputerScore + 1
+        elif UserChoice == "Scissors" and ComputerChoice == "Rock":
+            print("I won!")
+            time.sleep(0.5)
+            ComputerScore = ComputerScore + 1
+        elif UserChoice == "Scissors" and ComputerChoice == "Paper":
+            print("You won!")
+            time.sleep(0.5)
+            UserScore = UserScore + 1
+
+    print("End of game!")
+    time.sleep(1)
+    UserWantsToPlay = input("Do you want to play again? ")
+    if UserWantsToPlay in positiveOptions:
         time.sleep(0.5)
-        break
-        sys.exit()
-    elif UserChoice == "Rock" and ComputerChoice == "Paper":
-        print("I won!")
-        time.sleep(0.5)
-        break
-        sys.exit()
-    elif UserChoice == "Rock" and ComputerChoice == "Scissors":
-        print("You won!")
-        time.sleep(0.5)
-        break
-        sys.exit()
-    elif UserChoice == "Paper" and ComputerChoice == "Rock":
-        print("You won!")
-        time.sleep(0.5)
-        break
-        sys.exit()
-    elif UserChoice == "Paper" and ComputerChoice == "Scissors":
-        print("I won!")
-        time.sleep(0.5)
-        break
-        sys.exit()
-    elif UserChoice == "Scissors" and ComputerChoice == "Rock":
-        print("I won!")
-        time.sleep(0.5)
-        break
-        sys.exit()
-    elif UserChoice == "Scissors" and ComputerChoice == "Paper":
-        print("You won!")
+    else:
+        print("Goodbye.")
         time.sleep(0.5)
         break
         sys.exit()
