ec8e0a845304b5ee864336bfc5e008a6e31e533f
Tiago Ferreira <tiferrei2000@gmail.com>
2015-12-09 12:46:25 +0000

    Added Penalty Shootout

--- PenaltyShootout.py
+++ PenaltyShootout.py
@@ -15,10 +15,12 @@ Right = ["R", "r", "right", "Right"]
 ComputerHightOptions = ["High", "Low"]
 ComputerWidthOptions = ["Left", "Center", "Right"]
 Goals = 0
+savedGoals = 0
 
-for x in range(0,4):
-    KickHightInput = input(Where to do you want to kick the ball? (High, Low): )
-	KickWidthInput = input(Where to do you want to kick the ball? (Left, Centre, Right): )
+for x in range(0,5):
+
+    KickWidthInput = input("Where to do you want to kick the ball? (Left, Centre, Right): ")
+    KickHightInput = input("Where to do you want to kick the ball? (High, Low): ")
 
     if KickHightInput in High:
         KickHight = "High"
@@ -39,16 +41,20 @@ for x in range(0,4):
         sys.exit()
 
     ComputerRanHight = random.randint(0,1)
-    ComputerRanWidth = random.randint(0.2)
+    ComputerRanWidth = random.randint(0,2)
 
     ComputerHight = ComputerHightOptions[ComputerRanHight]
     ComputerWidth = ComputerWidthOptions[ComputerRanWidth]
 
     if ComputerWidth == KickWidth and ComputerHight == KickHight:
         print("Save! Aha!")
+        savedGoals = savedGoals + 1
     else:
         print("Gooooooal!")
         Goals = Goals + 1
 
 print("End of game!")
-print("You made " + Goals + " goals!")
+print("You made " + str(Goals) + " goals.")
+print("I saved " + str(savedGoals) + " goals.")
+
+#----------------------------------------------//----------------------------------------------
