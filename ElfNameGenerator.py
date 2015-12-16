526d44259522bfe3eb7501ee1dac212d8216e7c1
Tiago Ferreira <tiferrei2000@gmail.com>
2015-12-16 12:32:41 +0000

    Commented ElfNameGenerator.py

--- ElfNameGenerator.py
+++ ElfNameGenerator.py
@@ -12,46 +12,46 @@ positiveOptions = ["YES","yes","Yes","y","Y","Yup","yup","sim","s", "si", "sí",
                    "Oui", "oui", "Ja", "ja", "Ken", "ken", "sea", "Sea", "Shah", "shah",
                    "Jes", "jes", "Hai", "hai", "Ndiyo", "ndiyo", "是", "Shi", "shi"] #Created Array for possitive options.
 
-FirstElfNameArray = ["Sweetie","Jolly","Bubbles","Tootsie","Joyful","Sugar","Twinkle","Candy",
+FirstElfNameArray = ["Sweetie","Jolly","Bubbles","Tootsie","Joyful","Sugar","Twinkle","Candy", #Created Array for Elf first names.
                 "Merry","Huggy","Chipper","Angelic","Happy","Cookie","Sparkle","Jingle",
                 "Bouncy","Delightful","Treacle","Sprinkles","Cupcake","Tinsel","Perky",
                 "Precious","Sunny","Pinky"]
 
-LastElfNameArray = ["Twinkle-Toes","Sugar-Plum","Sparkle-Pants","McJingles","Peppermint-Buns",
+LastElfNameArray = ["Twinkle-Toes","Sugar-Plum","Sparkle-Pants","McJingles","Peppermint-Buns", #Created Array for Elf last name.
                "Sugar-Socks","Sprinkle-Pants","Angel-Ears","Sugar-Bells","Pointy-Toes",
                "McSprinkles","Candy-Canes"]
 
-Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", #Created Array for possible letters.
             "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-Months = ["January", "February", "March", "April", "May", "June", "July",
+Months = ["January", "February", "March", "April", "May", "June", "July", #Created Array for months.
           "August", "September", "October", "November", "December"]
 
-while UserReplay == True:
-    UserFirstName = input("Please enter your first name (John): ")
-    UserMonth = input("Please enter your Month of birth (July): ")
+while UserReplay == True: #Initiate loop.
+    UserFirstName = input("Please enter your first name (John): ") #Ask for first name.
+    UserMonth = input("Please enter your Month of birth (July): ") #Ask for month of birth.
 
-    UserFirstLetter = UserFirstName[0]
-
-
-
-    UserFirstLetterIndex = Letters.index(UserFirstLetter)
-    UserMonthIndex = Months.index(UserMonth)
-
-    FirstElfName = FirstElfNameArray[UserFirstLetterIndex]
-    LastElfName = LastElfNameArray[UserMonthIndex]
+    UserFirstLetter = UserFirstName[0] #Get the first letter of name.
 
 
-    ElfName = FirstElfName + " " + LastElfName
-    time.sleep(0.5)
-    print("Your elf name is " + ElfName)
-    time.sleep(0.5)
 
-    WantsToReplay = input("Do you want to generate another elf name? ")
+    UserFirstLetterIndex = Letters.index(UserFirstLetter) #Index it.
+    UserMonthIndex = Months.index(UserMonth) #Get Index for month.
 
-    if WantsToReplay in positiveOptions:
-        time.sleep(1)
-    else:
-        print("Goodbye, " + ElfName + "!")
-        time.sleep(1)
-        sys.exit()
+    FirstElfName = FirstElfNameArray[UserFirstLetterIndex] #Generate First elf name.
+    LastElfName = LastElfNameArray[UserMonthIndex] #Generate Last elf name.
+
+
+    ElfName = FirstElfName + " " + LastElfName #Combine them into one string.
+    time.sleep(0.5) #A bit of delay.
+    print("Your elf name is " + ElfName) #Prin the name.
+    time.sleep(0.5) #Another bit of delay.
+
+    WantsToReplay = input("Do you want to generate another elf name? ") #Ask if the user wants to replay.
+
+    if WantsToReplay in positiveOptions: #If yes,
+        time.sleep(1) #Wait a sencond and restart.
+    else: #If not,
+        print("Goodbye, " + ElfName + "!") #Say Goodbye.
+        time.sleep(1) #Wait a second.
+        sys.exit() #Exit.
