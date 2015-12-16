1337be3bbc6d07089fa5dff72d9ed183e94f48fb
Tiago Ferreira <tiferrei2000@gmail.com>
2015-12-16 11:51:54 +0000

    Created ElfNameGenerator.py

---
+++ ElfNameGenerator.py
@@ -0,0 +1,14 @@
+#Elf Name Generator
+
+#  Created by Tiago Ferreira on 16/12/2015.
+#  Copyright (c) 2015 Tiago Ferreira. All rights reserved.
+
+
+FirstElfName = ["Sweetie","Jolly","Bubbles","Tootsie","Joyful","Sugar","Twinkle","Candy",
+                "Merry","Huggy","Chipper","Angelic","Happy","Cookie","Sparkle","Jingle",
+                "Bouncy","Delightful","Treacle","Sprinkles","Cupcake","Tinsel","Perky",
+                "Precious","Sunny","Pinky"]
+
+LastElfName = ["Twinkle-Toes","Sugar-Plum","Sparkle-Pants","McJingles","Peppermint-Buns",
+               "Sugar-Socks","Sprinkle-Pants","Angel-Ears","Sugar-Bells","Pointy-Toes",
+               "McSprinkles","Candy-Canes"]
