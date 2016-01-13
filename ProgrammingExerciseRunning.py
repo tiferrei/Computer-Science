9f7e925a15e37201973a80d5c7ba274d3963ced9
Tiago Ferreira <tiferrei2000@gmail.com>
2016-01-13 14:14:39 +0000

    Updated copyright

--- ProgrammingExerciseRunning.py
+++ ProgrammingExerciseRunning.py
@@ -1,18 +1,17 @@
-NumberOfDays = int(input("How many days will you be walking for? "))
-NumberOfDays = NumberOfDays + 1
-Total = 0
+#Programming Exercise Runnung
 
-for x in range(1, NumberOfDays):
-    CurrentDay = input("Please enter the name of the day: ")
-    StagesForDay = int(input("Please enter the number of stages for " + CurrentDay + ": "))
-    StagesForDay = StagesForDay + 1
+#  Created by Tiago Ferreira on 13/01/2016.
+#  Copyright (c) 2016 Tiago Ferreira. All rights reserved.
 
-    for y in range(1, StagesForDay):
-        StartStage = int(input("Please enter the start position of stage " + str(y) + ": "))
-        EndStage = int(input("Please enter the end position of stage " + str(y) + ": "))
-
-        Total = Total + (EndStage - StartStage)
-
-print("Total distance walked: " + str(Total) + " kilometers.")
-WalkingTime = Total / 3
-print("Walking time: " + str(WalkingTime) + " hours.")
+Total = 0 #Set default value for distance
+NumberOfDays = int(input("How many days will you be walking for? ")) #Get number of days.
+for x in range(NumberOfDays): #Loop for number of days.
+    CurrentDay = input("Please enter the name of the day: ") #Get current day name.
+    StagesForDay = int(input("Please enter the number of stages for " + CurrentDay + ": ")) #How many stages in this day
+    for y in range(StagesForDay):#loopfor stages in a day
+        StartStage = int(input("Please enter the start position of stage " + str(y + 1) + ": "))#get start position
+        EndStage = int(input("Please enter the end position of stage " + str(y + 1) + ": "))#get end position
+        Total = Total + (EndStage - StartStage)#overall km calculation
+print("Total distance walked: " + str(Total) + " kilometers.")#print total distance
+WalkingTime = Total / 3 #Get walking time.
+print("Walking time: " + str(WalkingTime) + " hours.") #print walking time.
