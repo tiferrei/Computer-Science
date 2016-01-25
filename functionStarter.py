b7e998519a6cf4ccab53b5e3afbdebc0dc3bf5eb
Tiago Ferreira <tiferrei2000@gmail.com>
2016-01-25 12:09:31 +0000

    Started functions

---
+++ functionStarter.py
@@ -0,0 +1,25 @@
+#Function Starter
+
+#  Created by Tiago Ferreira on 25/01/2016.
+#  Copyright (c) 2015 Tiago Ferreira. All rights reserved.
+
+def summing(x,y):
+    total = x + y
+    print(total)
+    return total
+
+def domean(a,b):
+    mean = (a + b) / 2
+    print(mean)
+    return mean
+
+def subtract(c,d):
+    sub = (c - d)
+    print(sub)
+    return subtract
+
+summing(2,3)
+domean(2,4)
+subtract(5,2)
+
+# https://www.wetransfer.com/downloads/3646b76dfad6d052d428eb6225f5ec3120160125113143/58a78705135167cf9866434ad884460320160125113143/9dc04a#
