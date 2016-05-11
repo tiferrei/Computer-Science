3abad980fa36eb8f2b1146624c8e528e5376e1d2
Tiago Ferreira <me@tiferrei.com>
2016-05-11 12:24:17 +0100

    Added difficulty options

--- CSVCapitals.py
+++ CSVCapitals.py
@@ -12,25 +12,146 @@
 #  SOFTWARE.
 
 import csv
+import sys
+import os
+import time
+
 capitalQuestions = []
-points = 0
+hardQuestions = []
+mediumQuestions = []
+easyQuestions = []
+
 with open ("Capitals.txt") as csvfile:
     capitalsCsv = csv.reader(csvfile, delimiter = ",")
     for row in capitalsCsv:
         capitalQuestions.append(row)
+        if row[7] == "HARD":
+            hardQuestions.append(row)
+        elif row[7] == "MEDIUM":
+            mediumQuestions.append(row)
+        elif row[7] == "EASY":
+            easyQuestions.append(row)
 numberOfQuestions = len(capitalQuestions)
-for x in range(numberOfQuestions):
-    print(capitalQuestions[x][1])
+numberOfHardQuestions = len(hardQuestions)
+numberOfMediumQuestions = len(mediumQuestions)
+numberOfEasyQuestions = len(easyQuestions)
+
+def allQuestionsFunc():
+    points = 0
+    for x in range(numberOfQuestions):
+        print(capitalQuestions[x][1])
+        print()
+        print("A -> " + capitalQuestions[x][3])
+        print("B -> " + capitalQuestions[x][4])
+        print("C -> " + capitalQuestions[x][5])
+        print("D -> " + capitalQuestions[x][6])
+        print()
+        userChoice = input("Answer (A, B, C, D): ")
+        userChoice = userChoice.upper()
+        if userChoice == capitalQuestions[x][2]:
+            print("Correct!")
+            points = points + 1
+        else:
+            print("Wrong!")
+        time.sleep(2)
+        print()
+        os.system('clear')
+    print("You scored " + str(points) + " points!")
+
+def hardQuestionsFunc():
+    hardPoints = 0
+    for x in range(numberOfHardQuestions):
+        print(hardQuestions[x][1])
+        print()
+        print("A -> " + hardQuestions[x][3])
+        print("B -> " + hardQuestions[x][4])
+        print("C -> " + hardQuestions[x][5])
+        print("D -> " + hardQuestions[x][6])
+        print()
+        userChoice = input("Answer (A, B, C, D): ")
+        userChoice = userChoice.upper()
+        if userChoice == hardQuestions[x][2]:
+            print("Correct!")
+            hardPoints = hardPoints + 1
+        else:
+            print("Wrong!")
+        time.sleep(2)
+        print()
+        os.system('clear')
+    print("You scored " + str(hardPoints) + " points!")
+
+def mediumQuestionsFunc():
+    mediumPoints = 0
+    for x in range(numberOfMediumQuestions):
+        print(mediumQuestions[x][1])
+        print()
+        print("A -> " + mediumQuestions[x][3])
+        print("B -> " + mediumQuestions[x][4])
+        print("C -> " + mediumQuestions[x][5])
+        print("D -> " + mediumQuestions[x][6])
+        print()
+        userChoice = input("Answer (A, B, C, D): ")
+        userChoice = userChoice.upper()
+        if userChoice == mediumQuestions[x][2]:
+            print("Correct!")
+            mediumPoints = mediumPoints + 1
+        else:
+            print("Wrong!")
+        time.sleep(2)
+        print()
+        os.system('clear')
+    print("You scored " + str(mediumPoints) + " points!")
+
+def easyQuestionsFunc():
+    easyPoints = 0
+    for x in range(numberOfEasyQuestions):
+        print(easyQuestions[x][1])
+        print()
+        print("A -> " + easyQuestions[x][3])
+        print("B -> " + easyQuestions[x][4])
+        print("C -> " + easyQuestions[x][5])
+        print("D -> " + easyQuestions[x][6])
+        print()
+        userChoice = input("Answer (A, B, C, D): ")
+        userChoice = userChoice.upper()
+        if userChoice == easyQuestions[x][2]:
+            print("Correct!")
+            easyPoints = easyPoints + 1
+        else:
+            print("Wrong!")
+        time.sleep(2)
+        print()
+        os.system('clear')
+    print("You scored " + str(easyPoints) + " points!")
+
+def menu():
+    print("Welcome to the quiz! Choose an option below:")
     print()
-    print("A -> " + capitalQuestions[x][3])
-    print("B -> " + capitalQuestions[x][4])
-    print("C -> " + capitalQuestions[x][5])
-    print("D -> " + capitalQuestions[x][6])
-    userChoice = input("Answer (A, B, C, D): ")
-    userChoice = userChoice.upper()
-    if userChoice == capitalQuestions[x][2]:
-        print("Correct!")
-        points = points + 1
+    print("1 -> Easy questions")
+    print("2 -> Medium questions")
+    print("3 -> Hard questions")
+    print("4 -> All questions")
+    print("5 -> Quit")
+    print()
+    userSelection = input("Choose an option (1, 2, 3, 4, 5): ")
+    time.sleep(2)
+    if userSelection == "5":
+        os.system('clear')
+        sys.exit()
+    elif userSelection == "4":
+        os.system('clear')
+        allQuestionsFunc()
+    elif userSelection == "3":
+        os.system('clear')
+        hardQuestionsFunc()
+    elif userSelection == "2":
+        os.system('clear')
+        mediumQuestionsFunc()
+    elif userSelection == "1":
+        os.system('clear')
+        easyQuestionsFunc()
     else:
-        print("Wrong!")
-print("You scored " + str(points) + " points!")
+        print("Please choose a valid option.")
+        os.system('clear')
+
+menu()
