a43ba223d18bed0617089a785b072ff5d451ba60
Tiago Ferreira <me@tiferrei.com>
2017-09-12 13:23:36 +0100

    Added functions

--- A-Level/TicTacToe.py
+++ A-Level/TicTacToe.py
@@ -14,7 +14,10 @@
 import sys
 import time
 
-board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+possibleLines = [["00", "01", "02"], ["10", "11", "12"], ["20", "21", "22"],
+                 ["00", "10", "20"], ["01", "11", "21"], ["02", "12", "22"],
+                 ["00", "11", "22"], ["20", "11", "02"]]
 isComputerTurn = False
 computerUsesX = False
 
@@ -34,3 +37,7 @@ def isEMpty(row, column):
 def putPiece(piece, row, column):
     if isEmpty(row, column):
         board[row][column] = piece
+
+def preventOpponentLine():
+
+def AISelectPlace():
